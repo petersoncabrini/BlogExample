14815f09bd07f3667958458ac357bb02a74deb9b
Peterson <petersoncabrini@hotmail.com>
2023-04-08 10:01:12 -0300

    add image column to posts table

--- blog.backend/Models/Post.cs
+++ blog.backend/Models/Post.cs
@@ -9,6 +9,7 @@ namespace blog.backend.models
     public class Post
     {
         public Guid Id { get; set; }
+        public string Image { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Content { get; set; }
