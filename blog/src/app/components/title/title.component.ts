8da9e9f20aed6b587b881e19ef43a45c7c003a9f
Peterson <petersoncabrini@hotmail.com>
2023-04-09 11:07:43 -0300

    adicionar estilo ao título para as versoes light e dark mode

--- blog/src/app/components/title/title.component.ts
+++ blog/src/app/components/title/title.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
+
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss']
 })
-export class TitleComponent {
+export class TitleComponent implements OnInit {
+  isDarkMode: boolean;
 
+  constructor(private localStorage: StorageMap) {
+  }
+
+  ngOnInit(): void {
+    this.watchDarkModeEvent();
+  }
+
+  watchDarkModeEvent() {
+    this.localStorage.watch('isDarkMode', { type: 'boolean' }).subscribe(r => {
+      this.isDarkMode = r;
+      console.log(this.isDarkMode + "title")
+    })
+  }
 }
