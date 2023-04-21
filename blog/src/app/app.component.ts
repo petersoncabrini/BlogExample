ae84a8f1277526d58875c88f1c4e7bd11959f1eb
Peterson <petersoncabrini@hotmail.com>
2023-04-20 21:52:35 -0300

    adicionar módulos do angular material ao projeto

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -7,20 +7,13 @@ import { StorageMap } from '@ngx-pwa/local-storage';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'blog';
-  isDarkMode: boolean = false;
 
-  constructor(private localStorage: StorageMap) {
+  title = 'blog';
+
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.watchDarkModeEvent();
-  }
 
-  watchDarkModeEvent() {
-    this.localStorage.watch('isDarkMode', { type: 'boolean' }).subscribe(r => {
-      this.isDarkMode = r;
-    })
   }
-
 }
